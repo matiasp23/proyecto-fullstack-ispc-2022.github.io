c3352b132d97f75be64965483d7a9a02a0bb503f
FranGemo1 <franciscogemoo@gmail.com>
2022-11-05 20:28:05 -0300

    recuperando cambios

--- MVC/view/js/botonera.js
+++ MVC/view/js/botonera.js
@@ -1,32 +1,25 @@
-<<<<<<< HEAD
-$(document).ready(function(){
-    alert("absurdismo query")
-=======
-$(document).ready(function() {
-    // alert('"Hola absurdismo query')
->>>>>>> 6b191e019c4127a61bb60d31f56bd3d872df7e42
+// $(document).ready(function(){
+//     alert("absurdismo query")
 
-    //
-    $(".boton").click(function() {
-        let catEventos = $(this).attr("category");
-        console.log(catEventos);
+//
+$(".boton").click(function() {
+    let catEventos = $(this).attr("category");
+    console.log(catEventos);
 
-        //Ocultando Productos
+    //Ocultando Productos
 
-        $(".product").hide();
+    $(".product").hide();
 
-        //Mostrando Productos
+    //Mostrando Productos
 
-        $('.product[category="' + catEventos + '"]').show();
+    $('.product[category="' + catEventos + '"]').show();
 
-        $('.product[category="' + catEventos + '"]').addClass('.product')
+    $('.product[category="' + catEventos + '"]').addClass('.product')
 
-    });
+});
 
-    //Mostrando todos los productos
+//Mostrando todos los productos
 
-    $('.botontodos[category="todos"]').click(function() {
-        $('.product').show();
-    })
-
+$('.botontodos[category="todos"]').click(function() {
+    $('.product').show();
 });
