4c53a2a084bdd94ecdfe227ec61ba31431c318e7
Karen Pigni <64334155+Karen-Pigni@users.noreply.github.com>
2022-11-09 17:57:22 -0300

    conenccion a BBDD y Crud listo

--- back/model/conexion.py
+++ back/model/conexion.py
@@ -1,7 +1,7 @@
 import mysql.connector
 from mysql.connector import Error
 try:
-	connection=mysql.connector.connect(host='localhost',database='clasenterarte',user='root',password='')
+	connection=mysql.connector.connect(host='localhost',database='classenterarte',user='root',password='')
 	if connection.is_connected():
 			db_Info=connection.get_server_info()
 			print("Connected to MySQL Server version", db_Info)
